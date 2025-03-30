6d83ed555dde03e36c0ee9247e02401cce75d3b8
eliki <eliki13720@gmail.com>
2025-03-31 01:58:09 +0300

    testing the webhook2

--- _config.js
+++ _config.js
@@ -1,6 +1,7 @@
 var config = {}
 
 // Update to have your correct username and password
+//i have changed all
 config.mongoURI = {
     production: 'mongodb+srv://eliki:eliki13720@devops09-ip1.zrpcvub.mongodb.net/',
     development: 'mongodb+srv://eliki:eliki13720@devops09-ip1.zrpcvub.mongodb.net/',
